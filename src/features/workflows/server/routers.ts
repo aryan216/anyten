6ce29bdde242c35b8f5a5b47229beaf10e4acd51
aryan216 <aniketani603@gmail.com>
2025-11-13 19:04:23 +0530

    added payments setup and created workflows routes

---
+++ src/features/workflows/server/routers.ts
@@ -0,0 +1,61 @@
+import {generateSlug} from "random-word-slugs";
+import prisma from "@/lib/db";
+import { createTRPCRouter, premiumProcedure, protectecProcedure } from "@/trpc/init";
+import z from "zod";
+
+export const workflowsRouter = createTRPCRouter({
+  create: premiumProcedure.mutation(({ ctx }) => {
+    return prisma.workflow.create({
+      data: {
+        name: generateSlug(3),
+        userId: ctx.auth.user.id,
+      },
+    });
+  }),
+
+  remove: protectecProcedure
+  .input(z.object({ id: z.string() }))
+  .mutation(({ ctx,input }) => {
+    return prisma.workflow.delete({
+      where: {
+        id: input.id,
+        userId: ctx.auth.user.id,
+      },
+    });
+  }),
+
+  updateName: protectecProcedure
+  .input(z.object({ id: z.string(), name: z.string().min(1) }))
+  .mutation(({ ctx,input }) => {
+    return prisma.workflow.update({
+      where: {
+        id: input.id,
+        userId: ctx.auth.user.id,
+      },
+      data: {
+        name: input.name,
+      },
+    });
+  }),
+
+  getOne: protectecProcedure
+  .input(z.object({ id: z.string() }))
+  .query(({ ctx,input }) => {
+    return prisma.workflow.findUnique({
+      where: {
+        id: input.id,
+        userId: ctx.auth.user.id,
+      },
+    });
+  }),
+
+ getMany: protectecProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany({
+      where: {
+        userId: ctx.auth.user.id,
+      },
+    });
+  }),
+
+
+});
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
+import {checkout,polar,portal} from "@polar-sh/better-auth"
 import {betterAuth} from "better-auth";
 import { be } from "date-fns/locale";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import  prisma  from "@/lib/db";
+import {polarClient} from "./polar";
 
 export const auth = betterAuth({
    database: prismaAdapter(prisma,{
@@ -10,5 +12,22 @@ export const auth = betterAuth({
    emailAndPassword:{
     enabled:true,
     autoSignIn: true
-   }
+   },
+   plugins:[
+      polar({
+         client:polarClient,
+         createCustomerOnSignUp:true,
+         use:[
+            checkout({
+               products:[{
+                  productId:"108fe5c2-bca2-4a50-b589-49c313db441e",
+                  slug:"pro"
+               }],
+               successUrl:process.env.POLAR_SUCCESS_URL,
+               authenticatedUsersOnly:true
+            }),
+            portal({})
+         ]
+      }) 
+   ]
 }); 
